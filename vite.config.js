3bab035cc1833417410a393db784ce6c5e95f531
Ebrima220 <jallowe148@gmail.com>
2026-03-24 14:26:08 +0000

    made a git run

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        service: resolve(__dirname, 'service.html'),
+        gallery: resolve(__dirname, 'gallary.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
 })
